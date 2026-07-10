02794d691ca029ace43388630c6a41fc52b1cb04
sk-abhishek <skabhishek@icloud.com>
2026-07-10 22:42:16 +0530

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Flashcard Learning Aid</h1>
+      <p>My first React application 🎉</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header>
+        <h1>Flashcard Learning Aid</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
