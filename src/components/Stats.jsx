0e73cc2fcf42c3d4e31204b4b47605f613fd7be0
sk-abhishek <skabhishek@icloud.com>
2026-07-15 17:44:32 +0530

    create stats dashboard component

---
+++ src/components/Stats.jsx
@@ -0,0 +1,30 @@
+function Stats()
+{
+    return(
+        <section className="stats">
+
+            <div className="stat">
+                <h2>0</h2>
+                <p>Correct Answer</p>
+            </div>
+
+            <div className="stat">
+                <h2>0</h2>
+                <p>Incorrect Answers</p>
+            </div>
+
+            <div className="stat">
+                <h2>0%</h2>
+                <p>Accuracy</p>
+            </div>
+
+            <div className="stat">
+                <h2>0</h2>
+                <p>Current Streak</p>
+            </div>
+
+        </section>
+    );
+}
+
+export default Stats;
