3c1a2b907dd8e2f8192bf46401242e4a27ba7381
sk-abhishek <skabhishek@icloud.com>
2026-07-15 17:32:03 +0530

    create AnswerButtons component

---
+++ src/components/AnswerButtons.jsx
@@ -0,0 +1,17 @@
+function AnswerButtons()
+{
+    return (
+        <div className="answer-buttons">
+            <button className="correct">
+                I Knew It
+            </button>
+
+            <button className="wrong">
+                I Didn't Know
+            </button>
+        </div>
+
+    );
+}
+
+export default AnswerButtons;
