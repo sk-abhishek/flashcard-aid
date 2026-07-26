59a5ac7bff6430838fed14afea4ce67d40eabb48
sk-abhishek <skabhishek@icloud.com>
2026-07-26 18:44:23 +0100

    imported flashcards

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const flashcards = require("./data/flashcards");
+
 const app = express();
+
 const PORT = 3001;
 
 app.get("/", (req, res) => {
     res.send("Flashcard Aid Backend is Running");
 });
+
+app.get("/flashcards", (req, res) => {
+    res.json(flashcards);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
