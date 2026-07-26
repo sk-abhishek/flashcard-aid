db5a13ae46b79899a7b2e338863cd1f76ca3955d
sk-abhishek <skabhishek@icloud.com>
2026-07-26 18:21:37 +0100

    set up express backend with initial server config

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+    res.send("Flashcard Aid Backend is Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
