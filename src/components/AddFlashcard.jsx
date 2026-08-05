9a14aff7248e7d26a2bf256cf9b9d91bc73eb020
sk-abhishek <skabhishek@icloud.com>
2026-08-05 09:46:17 +0100

    fixing add flashcard functionality

--- src/components/AddFlashcard.jsx
+++ src/components/AddFlashcard.jsx
@@ -64,7 +64,7 @@ function AddFlashcard({ setFlashcards }) {
             // update React state so UI updates immediately
             setFlashcards(prev => [
                 ...prev,
-                data
+                data.flashcard
             ]);
 
             // clear form
